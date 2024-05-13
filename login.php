44c3ebc13bd9e636ddad87a937c0708d125fbb56
efeince0 <168727450+efeince0@users.noreply.github.com>
2024-05-13 22:38:18 +0300

    Add files via upload
    login güncellemleri

---
+++ login.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+
+// Post edilen verileri al
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// Kullanıcı adı ve şifreyi kontrol et
+if (!empty($username) && !empty($password)) {
+        // Kullanıcı adını kontrol et
+        if ($username === "b1812100001@sakarya.edu.tr") {
+            // Şifreyi kontrol et
+            if ($password === "b1812100001") {
+                // Giriş başarılı
+                $_SESSION['username'] = $username;
+                header("Location: index.html");
+                exit;
+            } else {
+                // Şifre hatalı
+                header("Location: login.html");
+                exit;
+            }
+        } else {
+            // Kullanıcı adı hatalı
+            header("Location: login.html");
+            exit;
+        }
+    } else {
+        // Kullanıcı adı veya şifre boş
+        header("Location: login.html");
+        exit;
+    }
+    ?>
